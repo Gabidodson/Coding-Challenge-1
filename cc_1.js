1c298aa3a5e56954077a7a7956bedfd76596afcd
Gabriela Dodson <dodsongabi@gmail.com>
2024-08-31 09:46:06 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+//Task 1: Variables and Data Types
+
+let employeeName= "Charles Darwin";
+const employeeID= "7";
+var isActive = true;
+console.log(employeeName,typeof (employeeName));
+console.log(employeeID,typeof(employeeID));
+console.log(isActive,typeof(isActive));
