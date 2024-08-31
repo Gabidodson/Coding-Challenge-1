c18120b40ebf7f604e647ee5f161024a785e431e
Gabriela Dodson <dodsongabi@gmail.com>
2024-08-31 10:11:00 -0400

    Task 2-Produvt Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,15 @@ var isActive = true;
 console.log(employeeName,typeof (employeeName));
 console.log(employeeID,typeof(employeeID));
 console.log(isActive,typeof(isActive));
+
+
+//Task 2: Compound Data Types
+
+let product = ["dog-collar","food","treats"];
+const productDetails={
+    names: product,
+    inStock:10,
+    price:30.00,
+}
+console.log(product,productDetails);
+
